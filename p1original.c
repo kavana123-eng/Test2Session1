913056d7aa302c88162dc2e54b53f884c20f06af
Kavana Bankapur <97089741+kavana123-eng@users.noreply.github.com>
2022-03-16 04:26:42 +0000

    completed

--- p1original.c
+++ p1original.c
@@ -3,19 +3,19 @@
 void input(float *x1, float *y1, float *x2, float *y2)
 {
   printf("Enter The Points \n");
-  Scanf(""%f%f%f%f",x1,x2,y1,y2);
+  scanf("%f%f%f%f",x1,x2,y1,y2);
 }
 void find_distance(float x1, float y1, float x2, float y2, float *distance)
 {
   *distance=sqrt(pow((x2-x1),2)+ pow((y2-y1),2));
 }
-void output(float x1, float y1, float x2, float y2,float x3, float y3, int istriangle)
+void output(float x1, float y1, float x2, float y2, float distance)
 {
   printf("the distance between the points (%f,%f) and (%f,%f) is %f \n ",x1,y1,x2,y2,distance);
 }
 int main()
 {
- float x1,y1.x2,y2,d;
+ float x1,y1,x2,y2,d;
   input(&x1,&y1,&x2,&y2);
   find_distance(x1,y1,x2,y2,&d);
   output(x1,y1,x2,y2,d);
---
+++ p5original.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int input_array_size()
+{
+  int n;
+  printf("enter the number");
+  scanf("%d",&n);
+  return n;
+}
+void init_array(int n, int a[n]
+{
+  int 1;
+  for(i=0;i<n;i++)
+    {
+      a[i]=i+1;
+    }
+}
