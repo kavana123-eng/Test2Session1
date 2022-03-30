3669a4cd3972979c4c81990fd0a921828c87ed52
Kavana Bankapur <97089741+kavana123-eng@users.noreply.github.com>
2022-03-30 11:47:48 +0000

    completed

--- p2original.c
+++ p2original.c
@@ -2,7 +2,7 @@
 #include <math.h>
 void input_line(float *x1, float *y1)
 {
-  printf("enter the values of x,y\n");
+   printf("enter the values of x,y\n");
   scanf("%f%f",x1,y1);
 }
 int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3)
